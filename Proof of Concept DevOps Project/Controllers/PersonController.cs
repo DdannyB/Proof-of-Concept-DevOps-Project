17ba0abc7b5402df9f5952e61a259f0f466875ac
Danny Brouwers <dannybrouwers@live.nl>
2018-03-19 16:08:15 +0100

    Add project files.

---
+++ Proof of Concept DevOps Project/Controllers/PersonController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Proof_of_Concept_DevOps_Project.Models;
+
+namespace Proof_of_Concept_DevOps_Project.Controllers
+{
+    public class PersonController : Controller
+    {
+        public static List<Person> persons = new List<Person>();
+        public static Person persoon = new Person();
+
+        private IActionResult RedirectToHome()
+        {//Terug gaan naar de Index pagina.
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Index()
+        {//Het ophalen van de persoon gegevens in de api.
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:51055/api/person");
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                String responseString = await response.Content.ReadAsStringAsync();
+                persons = JsonConvert.DeserializeObject<List<Person>>(responseString);
+                return View(persons);
+            }
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {//Het scherm voor het toevoegen van een persoon
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person p)
+        {//De nieuwe persoon toevoegen aan de list in de api.
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(p), System.Text.Encoding.UTF8, "application/json");
+
+            HttpClient client = new HttpClient();
+            var post = client.PostAsync("http://localhost:51055/api/person/", content);
+            post.Wait();
+            var result = post.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToHome();
+            }
+
+            return View(p);
+        }
+
+        public ActionResult Edit(int Id)
+        {//het openen van de edit pagina en de gegevens van de juiste persoon inladen
+            persoon = persons.Where(x => x.Id == Id).FirstOrDefault();
+            return View(persoon);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Person p)
+        {//De gegevens die aangepast moeten worden doorsturen naar de api.
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(p), System.Text.Encoding.UTF8, "application/json");
+
+            HttpClient client = new HttpClient();
+            var post = client.PutAsync("http://localhost:51055/api/person/" + p.Id, content);
+            post.Wait();
+            var result = post.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToHome();
+            }
+
+            return View(p);
+        }
+
+        public ActionResult Delete(int Id)
+        {//Het scherm laden om de geselecteerde persoon te verwijderen.
+            persoon = persons.Where(x => x.Id == Id).FirstOrDefault();
+            return View(persoon);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Person p)
+        {//De geselecteerde persoon verwijderen uit de list in de api.
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(p), System.Text.Encoding.UTF8, "application/json");
+
+            HttpClient client = new HttpClient();
+            var post = client.DeleteAsync("http://localhost:51055/api/person/" + p.Id);
+            post.Wait();
+            var result = post.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToHome();
+            }
+
+            return View(p);
+        }
+    }
+}
+    
---
+++ Proof of Concept DevOps Project/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proof_of_Concept_DevOps_Project.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
