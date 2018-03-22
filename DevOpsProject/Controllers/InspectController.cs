e7327c9f1c35e0e6715e1611a80e6094353af165
Danny Brouwers <dannybrouwers@live.nl>
2018-03-22 10:50:05 +0100

    added inspect class to check person controller request added docker compose file

---
+++ DevOpsProject/Controllers/InspectController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    public class InspectController : Controller
+    {
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(string url = "http://webapicontainerrrr/api/person/")
+        {
+            ViewBag.Url = url;
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    ViewBag.ErrorMessage = response.ReasonPhrase;
+                }
+                ViewBag.StatusCode = response.StatusCode;
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.ErrorMessage = ex.Message;
+            }
+            return View();
+        }
+    }
+}
--- DevOpsProject/Controllers/PersonController.cs
+++ DevOpsProject/Controllers/PersonController.cs
@@ -13,7 +13,7 @@ namespace DevOpsProject.Controllers
     {
         public static List<Person> persons = new List<Person>();
         public static Person persoon = new Person();
-        public static string uri = "http://gifted_ride/api/person/";
+        public static string uri = "http://webapicontainerrrr/api/person/";
 
         private IActionResult RedirectToHome()
         {//Terug gaan naar de Index pagina.
